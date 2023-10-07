9d6a49aac328df0d766947fdce0793f93a6a88e6
Yiang Liu <liu-ya20@mails.tsinghua.edu.cn>
2023-10-07 11:55:06 +0800

    Add files via upload

---
+++ midi/test.py
@@ -0,0 +1,10 @@
+# try:
+#     import fluidsynth
+#     _HAS_FLUIDSYNTH = True ##
+# except ImportError:
+#     print('ji')
+
+import os
+# for directory in os.environ['fluidsynth'].split(os.pathsep):
+#     print(directory)
+print(os.environ['HOME'])
